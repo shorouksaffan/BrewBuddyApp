2163219f8b9747af87b95c6a5229c99adac7a18a
Eslam <eslamm378@gmail.com>
2025-08-29 19:58:07 +0300

    Add Room DAOs and entities for drink caching, favorites, and orders

---
+++ app/src/main/java/com/example/brewbuddy/core/data/remote/ApiResult.kt
@@ -0,0 +1,6 @@
+package com.example.brewbuddy.core.data.remote
+
+sealed class ApiResult<out T> {
+    data class Success<out T>(val data: T) : ApiResult<T>()
+    data class Failure(val exception: Exception) : ApiResult<Nothing>()
+}
